8c09b579b186d273b88d1d56a686d1be22f524b8
gktnwls1127 <gktnwls1127@naver.com>
2020-07-08 17:24:28 +0900

    th

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { response } from 'express'
+import ProductImage from './Sections/ProductImage'
+import ProductInfo from './Sections/ProductInfo'
+import { Row, Col } from 'antd'
 
-function DetailProductPAge(props) {
+function DetailProductPage(props) {
 
     const productId = props.match.params.productId
+
+    const [Product, setProduct] = useState({})
 
     useEffect(()=>{
         axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
             .then(response => {
                 if(response.data.success) {
-                    
+                    setProduct(response.data.product[0])
                 } else {
                     alert('상세정보 가져오기를 실패했습니다.')
                 }
@@ -18,10 +22,34 @@ function DetailProductPAge(props) {
     },[])
 
     return (
-        <div>
+        <div style={{ width: '100%', padding: '3rem 4rem' }}>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <h1>{Product.title}</h1>
+            </div>
+
+            <br />
+
+            <Row gutter={[16, 16]}>
+
+                <Col lg={12} sm={24}>
+                
+                    {/* ProductImage */}
+                    
+                    <ProductImage detail={Product}/>
+
+                </Col>
+                <Col lg={12} sm={24}>
             
+                    {/* ProductInfo */}
+
+                    <ProductInfo detail={Product}/>
+                
+                </Col>
+
+            </Row>
+
         </div>
     )
 }
 
-export default DetailProductPAge
+export default DetailProductPage
