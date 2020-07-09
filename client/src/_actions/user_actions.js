50547135fd9a7291619950e80e05ee1b3b444376
gktnwls1127 <gktnwls1127@naver.com>
2020-07-09 16:02:28 +0900

    five

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -5,7 +5,9 @@ import {
     AUTH_USER,
     LOGOUT_USER,
     ADD_TO_CART,
-    GET_CART_ITEMS
+    GET_CART_ITEMS,
+    REMOVE_CART_ITEM,
+    ON_SUCCESS_BUY
 } from './types';
 import { USER_SERVER } from '../components/Config.js';
 
@@ -64,25 +66,25 @@ export function addToCart(id){
     }
 }
 
-export function getCartItems(cartItems, userCart){
-    const request = axios.get(`api/product/products_by_id?id=${cartItems}&type=array`)
-    .then(response => {        
+export function getCartItems(cartItems, userCart) {
+    const request = axios.get(`/api/product/products_by_id?id=${cartItems}&type=array`)
+        .then(response => {    
             // CartItem들에 해당하는 정보들을 
             // Product Collection에서 가져온 후에 
             // Quantity 정보를 넣어준다.
 
             userCart.forEach(cartItem => {
-                response.data.product.forEach((productDetail, index) => {
-                    if(cartItem.id === productDetail._id) {
-                        response.data.product[index].quantity = cartItem.quantity
+                response.data.forEach((productDetail, i) => {
+                    if (cartItem.id === productDetail._id) {
+                        response.data[i].quantity = cartItem.quantity;
                     }
-
                 })
             })
 
-            return response.data.product;
+            return response.data;
 
     });
+    
 
     return {
         type: GET_CART_ITEMS,
@@ -90,4 +92,36 @@ export function getCartItems(cartItems, userCart){
     }
 }
 
+export function removeCartItem(productId) {
+    const request = axios.get(`/api/users/removeFromCart?_id=${productId}`)
+        .then(response => {    
+     
+            //productInfo, cart 정보를 조합해서 CartDetail을 만든다
+            response.data.cart.forEach(item => {
+                response.data.productInfo.forEach((product, index) => {
+                    if(item.id === product._id) {
+                        response.data.productInfo[index].quantity = item.quantity
+                    }
+
+                })
+            })
+            return response.data;
+
+    });
+
+    return {
+        type: REMOVE_CART_ITEM,
+        payload: request
+    }
+}
+
+export function onSuccessBuy(data) {
+    const request = axios.post(`/api/users/successBuy`, data)
+        .then(response =>  response.data);
+
+    return {
+        type: ON_SUCCESS_BUY,
+        payload: request
+    }
+}
 
--- client/src/components/views/CartPage/Sections/UserCardBlock.js
+++ client/src/components/views/CartPage/Sections/UserCardBlock.js
@@ -8,22 +8,27 @@ function UserCardBlock(props) {
             let image = images[0]
             return `http://localhost:5000/${image}`
         }
-    }
+    }   
 
     const renderItems = () => (
-        props.products && props.products.map(product => (
-            <tr key={product._id}>
+        props.products && props.products.map((product, index) => (
+            <tr key={index}>
                 <td>
                     <img style={{ width: '70px' }} alt="product" 
                     src={renderCartImage(product.images)} />
                 </td> 
                 <td>{product.quantity} EA</td>
                 <td>$ {product.price} </td>
-                <td><button> Remove </button> </td>
+                <td>
+                    <button onClick={() => props.removeItem(product._id)}>
+                         Remove 
+                    </button> 
+                </td>
             </tr>
         ))
     )
-    
+
+
     return (
         <div>
             <table>
@@ -35,9 +40,11 @@ function UserCardBlock(props) {
                         <th>Remove from Cart</th>
                     </tr>
                 </thead>
+
                 <tbody>
-                    {renderItems}
+                    {renderItems()}
                 </tbody>
+                
             </table>
         </div>
     )
